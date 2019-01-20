5699799af2b3d283589b2c05785e5ab4321ec8d7
markhogue <mark.hogue.chp@gmail.com>
2019-01-20 16:35:25 -0500

    Removing ICRP data and associated functions since the large package including the data failed to load.

--- data-raw/load.install.data.R
+++
@@ -1,5 +0,0 @@
-bet.df <- readr::read_tsv("C:/R/radsafer/data-raw/bet.dat.tsv")
-usethis::use_data(bet.df, compress = "xz")
-
-RN.df <- readr::read_tsv("C:/R/radsafer/data-raw/RN.dat.tsv")
-usethis::use_data(RN.df, compress = "xz")
