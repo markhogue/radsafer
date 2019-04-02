d9f400beaa1b89f22d8c27b6f65824c31fdc6f46
markhogue <mark.hogue.chp@gmail.com>
2019-04-01 22:02:22 -0400

    unit name change in t/s error

--- R/mcnp_plot_out_spec.R
+++ R/mcnp_plot_out_spec.R
@@ -1,13 +1,13 @@
 #' Convert histogram data to average points and plot as spectrum.
 #' @description Model results from MCNP in energy bins with   maximum energy in MeV in the first column and the mean result for the bin in the second column.
 #' @param spec.df A data frame with no header. Maximum energy in MeV should be in the first column, binned results in the second column, uncertainty (optionally) in the third column.
-#' @param units Result units   
+#' @param rad_units Result units   
 #' @param title Title for chart
 #' @examples
 #' library(ggplot2)
 #' plot_spec_from_hist(photons_cs137_hist[1:16, ], "mR_h", "Example: Cs-137 with scatter")
 #' @export
-plot_spec_from_hist <- function(spec.df, units, title) {
+plot_spec_from_hist <- function(spec.df, rad_units, title) {
     if (!requireNamespace("ggplot2", quietly = TRUE)) {
     stop("Package \"ggplot2\" needed for this function to work. Please install it.",
            call. = FALSE)
@@ -20,7 +20,7 @@ plot_spec_from_hist <- function(spec.df, units, title) {
     ggplot2::geom_point(shape = 8) +
     ggplot2::geom_line(color = 'gray') +
     ggplot2::xlab("Energy, MeV") +
-    ggplot2::ylab(paste0("relative ", units)) + 
+    ggplot2::ylab(paste0("relative ", rad_units)) + 
     ggplot2::ggtitle(paste0(title))
 }
 
