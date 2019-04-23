8201d53b3461842f18e4eed37542a9aa35f79db1
markhogue <mark.hogue.chp@gmail.com>
2019-04-23 18:26:36 -0400

    Add space before dist_type. Delete phantom duplicate functions.

--- R/mcnp_si_sp_RD.R
+++ R/mcnp_si_sp_RD.R
@@ -121,7 +121,7 @@ mcnp_si_sp_RD <- function(desired_RN, rad_type = NULL, photon = FALSE,
   r_type_text <- as.character(rad_type)
   if (photon == TRUE) 
     r_type_text <- "photon"
-  utils::write.table(data.frame(text = paste0("si", erg.dist, dist_type, " & ", "$", desired_RN, ", radiation type= ", r_type_text)), "si.sp.dat", col.names = FALSE, row.names = FALSE, append = TRUE, quote = FALSE)
+  utils::write.table(data.frame(text = paste0("si", erg.dist," ", dist_type, " & ", "$", desired_RN, ", radiation type= ", r_type_text)), "si.sp.dat", col.names = FALSE, row.names = FALSE, append = TRUE, quote = FALSE)
   utils::write.table(data.frame(text = paste0("c  energy cut off at ", cut, " MeV ")), "si.sp.dat", col.names = FALSE, row.names = FALSE, append = TRUE, quote = FALSE)
   readr::write_delim(data.frame(si), "si.sp.dat", col_names = FALSE, append = TRUE)
   # sp output
@@ -131,36 +131,3 @@ mcnp_si_sp_RD <- function(desired_RN, rad_type = NULL, photon = FALSE,
   readr::write_delim(data.frame(sp), "si.sp.dat", col_names = FALSE, append = TRUE)
 }
 
-#' MCNP histograms source energies
-#' @description Make MCNP histogram energy bins cards for source definition if inputs happen to be available in histogram format.
-#' @param emin A vector of energies forming the lower bound of the bin. (emin values other than the first value also provide a bin emax.)
-#' @param emax A single energy with the upper bound of the highest bin. 
-#' @return A matrix of values for source input to copy and paste into an MCNP input. (The # should be changed to the appropriate distribution number. The NA's in the last row should be discarded.)
-#' @examples 
-#' si_hist(1:10 / 10, 1.2)
-#' @export
-#  ------------------------
-si_hist <-  function(emin, emax) {
-  X <- c(emin, emax[length(emax)])
-  if(length(X) %% 5 !=0) X <- c(X, rep(NA, 5 -length(X) %% 5))
-  prmatrix(matrix(X, byrow = T, ncol = 5), 
-           rowlab = c("si#  ", rep("     ", length(X)/5 - 1)),
-           collab = rep("     ", 5))
-}
-
-#' MCNP histograms source bin probabilities
-#' @description Make MCNP histogram probabilities for energy bins.
-#' @param bin_prob A vector of the bin probabilities.
-#' @return A matrix of values for source probabilities to copy and paste into an MCNP input. (The # should be changed to the appropriate distribution number. The NA's in the last row should be discarded.)
-#' @examples 
-#' sp_hist(rep(1 / 11, 11))
-#' @export
-#  ------------------------
-
-sp_hist <- function(bin_prob) {
-  X <- c(0, bin_prob) / sum(bin_prob) # What's below the first bin - si's are max for bin
-  if(length(X) %% 5 != 0) X <- c(X, rep(NA, 5 -length(X) %% 5))
-  prmatrix(matrix(X, byrow = T, ncol = 5), 
-           rowlab = c("sp#  ",rep("     ", length(X)/5 - 1)),
-           collab = rep("     ", 5))
-}
